31f5d25bb8ac25ee41ebef464fed9ad4973ce3b6
Pelle Wessman <pelle@kodfabrik.se>
2015-07-02 17:56:07 +0200

    chore(main): fix Travis coverage reports

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,8 +4,22 @@
 var lintlovin = require('lintlovin');
 
 module.exports = function (grunt) {
-  lintlovin.initConfig(grunt, {}, {
-    enableCoverageEvent: true
+  lintlovin.initConfig(grunt, {
+    mocha_istanbul : {
+      options: {
+        ui: 'tdd',
+        coverage: true,
+        reportFormats: ['lcov']
+      },
+      basic: {
+        src: process.env.TRAVIS ? ['test/**/*.js'] : ['test/**/*.js', '!test/integration/**/*.js']
+      },
+      integration: {
+        src: ['test/integration/**/*.js']
+      }
+    }
+  }, {
+    integrationWatch: true
   });
 
   grunt.event.on('coverage', function (lcov, done) {
